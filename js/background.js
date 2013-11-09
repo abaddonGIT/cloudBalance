7f676c95b51b5b7aba1e6704ca852fe1ba91a6fc
abaddonGIT <abaddongit@gmail.com>
2013-11-10 03:30:40 +0400

    Переписан на шаблоне стратегия. Добавлен маленький шаблонизатор для сообщений

--- js/background.js
+++ js/background.js
@@ -2,36 +2,101 @@
  * Copyright 2013 by Abaddon <abaddongit@gmail.com>
  * @author Abaddon <abaddongit@gmail.com>
  * @version 1.0.0
- * ***************************************************/
-var w = window;
-var Balance = function () {
+ * **************************************************/
+var Balancer = function (strategy) {
+    this.strategy = strategy;
+};
+
+Balancer.prototype.start = function () {
+    return this.strategy.init();
+};
+//Что-то типа абстрактного класса
+var GetBalanceStrategy = function () {};
+//Методы используемые с стратегиях
+
+//Запуск
+GetBalanceStrategy.prototype.init = function () {
+    this.messagesText = {
+        balance: {
+            title: "Ваш баланс на Selectel ниже установленного уровня",
+            message: "Ваш баланс {{b.allSum}} руб. Баланс облака {{b.cloudBal}} руб. Предпологаемый расход на день {{b.predict}} руб."
+        },
+        warning: {
+            title: "Произошла ошибка!!!",
+            message: "Перед использованием необходимо заполнить информацию в настройках!"
+        }
+    };
+    //Вытаскиваем данные из хранилща
+    if (localStorage['options'] !== undefined) {
+        var data = JSON.parse(localStorage['options']);
+        this.authCloud(data);
+    } else {
+        this.showMessage({
+            type: "basic",
+            title: this.messagesText.warning.title,
+            message: this.messagesText.warning.message,
+            iconUrl: "../img/warning_128.png"
+        }, 'error', 'warning');
+    }
+};
+//Авторизация в облаке
+GetBalanceStrategy.prototype.authCloud = function (data) {};
+//запрос баланса
+GetBalanceStrategy.prototype.getCloudInfo = function (data) {};
+
+GetBalanceStrategy.prototype.crypt = function (text, step) {
+    total = '', ln = text.length;
+    for (var i = 0; i < ln; i++) {
+        total += String.fromCharCode(text.charCodeAt(i) + step);
+    }
+    return total;
+};
+//шабланизаторик
+GetBalanceStrategy.prototype.parseTpl = function (obj, template) {
+    var str = "", f;
+
+    str = "var out='" + template.replace(/\{\{([\s\S]+?)\}\}/g, function (m, code) {
+        return "' + (" + unescape(code) + ") + '";
+    }) + "'; return out;";
+
+    f = new Function('b', str);
+    return f(obj);
+};
+//создание оповещения
+GetBalanceStrategy.prototype.showMessage = function (config, ikonText, name) {
+    chrome.browserAction.setBadgeText({ "text": ikonText + '' });
+
+    chrome.notifications.getAll(function (notifications) {
+        if (notifications[name]) {
+            chrome.notifications.clear(name, function (wasCleared) {
+                chrome.notifications.create(name, config, function () { });
+            });
+        } else {
+            chrome.notifications.create(name, config, function () { });
+        }
+    });
+};
+
+//Описываем стратегии для разных сервисов
+
+//Для Selectel
+var GetBalanceStategyFromSelectel = function () {
     this.config = {
-        1: {
-            request: 'https://support.selectel.ru/api/login',
-            balanceUrl: 'https://support.selectel.ru/api/balance',
-            notificationOpt: {
-                type: "basic",
-                title: "",
-                message: "",
-                iconUrl: "../img/clouds_128.png"
-            }
+        request: 'https://support.selectel.ru/api/login',
+        balanceUrl: 'https://support.selectel.ru/api/balance',
+        notificationOpt: {
+            type: "basic",
+            title: "",
+            message: "",
+            iconUrl: "../img/warning_128.png"
         },
         step: 2
-    }
-
-    var config = this.config;
-
-    this.init = function () {
-        if (localStorage['options'] !== undefined) {
-            this.data = JSON.parse(localStorage['options']);
-            //отправляем запрос
-            this.getCookies();
-        }
-    }
-    //авторизация на облаке
-    this.getCookies = function () {
-        var xhr = new XMLHttpRequest(), data = this.data;
-        link = config[data.cloud]['request'];
+    };
+    var config = this.config, gb = this;
+    //Авторизация для селектела
+    this.authCloud = function (data) {
+        var xhr = new XMLHttpRequest();
+        link = config['request'];
 
         xhr.open('POST', link, true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -39,57 +104,42 @@ var Balance = function () {
             if (this.status == 200) {
                 var response = JSON.parse(this.response);
                 if (response.status === "ok") {//Если авторизация прошла то запускаем ф-ю опроса баланса
-                    bal.getBalance(); //Сразу выводим оповещение
-                    setInterval(bal.getBalance, bal.data.frequency*1000);
+                    //Запрашиваем информацию и выводим оповещение
+                    gb.getCloudInfo(data);
+                    setInterval(function () {
+                        gb.getCloudInfo(data);
+                    }, data.frequency * 1000);
                 }
             }
         }
-        xhr.send('login=' + data.contract + '&password=' + bal.crypt(data.password, -config.step));
-    }
+        xhr.send('login=' + data.contract + '&password=' + this.crypt(data.password, -config.step));
+    };
 
-    this.getBalance = function () {
+    this.getCloudInfo = function (data) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', config[bal.data.cloud]['balanceUrl'], true);
+        xhr.open('GET', config['balanceUrl'], true);
         xhr.onload = function (e) {
             if (this.status == 200) {
                 var response = JSON.parse(this.response);
                 if (response.status === "ok") {
                     var allSum = parseInt(response.balance, 10) / 100, cloudBal = parseInt(response.cloud.balance) / 100;
 
-                    if (cloudBal < bal.data.price) {//Если ниже заданного порога врубаем уведомления
-                        config[bal.data.cloud]['notificationOpt'].message = "Ваш баланс " + allSum + ' руб. Баланс облака ' + cloudBal + ' руб. Предпологаемый расход на день ' + response.cloud.predict + ' руб.';
-                        config[bal.data.cloud]['notificationOpt'].title = bal.data.title || "Ваш баланс на Selectel ниже установленного уровня"
-
-                        chrome.notifications.getAll(function (notifications) {
-                            if (notifications.balance) {
-                                chrome.notifications.clear('balance', function (wasCleared) {
-                                    chrome.notifications.create('balance', config[bal.data.cloud]['notificationOpt'], function () { });
-                                });
-                            } else {
-                                chrome.notifications.create('balance', config[bal.data.cloud]['notificationOpt'], function () { });
-                            }
-                        });
+                    if (cloudBal < data.price) {//Если ниже заданного порога врубаем уведомления
+                        config['notificationOpt'].message = gb.parseTpl({'allSum': allSum, 'cloudBal': cloudBal, 'predict': response.cloud.predict}, gb.messagesText.balance.message);
+                        config['notificationOpt'].title = data.title || gb.messagesText.balance.title;
+                        //выводим оповещения
+                        gb.showMessage(config['notificationOpt'], cloudBal, 'balance');
                     }
                 }
             }
         }
         xhr.send();
-    };
-
-    var bal = this;
-}
-
-Balance.prototype.crypt = function (text, step) {
-    total = '', ln = text.length;
-
-    for (var i = 0; i < ln; i++) {
-        total += String.fromCharCode(text.charCodeAt(i) + step);
     }
-
-    return total;
-};
+}
+//Наследуем методы для стратегии
+GetBalanceStategyFromSelectel.prototype = Object.create(GetBalanceStrategy.prototype);
 
 window.onload = function () {
-    var balance = new Balance();
-    balance.init();
+    var balance = new Balancer(new GetBalanceStategyFromSelectel());
+    balance.start();
 }
